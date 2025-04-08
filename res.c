2388cd01432059f5e8ad2bf490202d16124af551
Matteo-Guicheneuy <matteo.guicheneuy@ens-paris-saclay.fr>
2025-04-09 01:46:40 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   res.c
    
     Untracked files:
    	.vscode/
    	Exp_Mellin.txt
    	Exp_NLOXsec.txt
    	Exp_NLOXsec_Mel.txt
    	Exp_NLO_errXsec.txt
    	Exp_NLO_errXsec_Mel.txt
    	ExpandedXsec.txt
    	LOXsec.txt
    	Madgraph NLO.txt
    	NL0_Exp_Col_imp.txt
    	NLOXsec.txt
    	NLOXsec_Mel.txt
    	NLO_Exp.txt
    	NLO_Exp_no_C.txt
    	RUN
    	ResumXsec_Analitic.txt
    	ResumXsec_C.txt
    	ResumXsec_Mellin.txt
    	Resum_ExpXsec.txt
    	Test Mellin NLO and x.txt
    	Test Pdf anzat esp E 3.txt
    	Test a.txt
    	Test alpha/
    	Test threshold/
    	With charm quark and no.txt
    	a 1 C 2 05.txt
    	all.txt
    	derivation PDF 2 precision.c
    	inc/main.h
    	inc/polygamma.h
    	int_res.txt
    	test.txt
    	test2.txt
    	test3.txt
    	test_Exp_NLO_CI.txt
    	test_NLO_MG.txt
    	test_mycode_Yehudi.txt
    	testx.txt
    	testy.txt
    	testybis.txt
    	with_charm_pdf_evol.txt
    	without_charm.txt

---
+++ res.c
@@ -0,0 +1,58 @@
+eps=E-3
+ic=0
+////incertitude = 2.09845 res1 = 46.3995 res2 = 46.3567 err1 = 0.003505 err2 = 0.0200994////
+kID = 3, duration=0.015470s, sc = 300.000000.
+kID = 4, duration=49.262726s, sc = 300.000000.
+
+ic=1
+////incertitude = 0.392116 res1 = 1018.81 res2 = 1018.5 err1 = 0.0016721 err2 = 0.783795////
+kID = 3, duration=0.014112s, sc = 675.163770.
+kID = 4, duration=242.324645s, sc = 675.163770.
+/ / / / / /   CUT / / / / / / / 
+ic=2
+////incertitude = 2.62812 res1 = 5145.19 res2 = 5140.43 err1 = 0.00777328 err2 = 1.81203////
+kID = 3, duration=0.013194s, sc = 1519.487052.
+kID = 4, duration=1.976098s, sc = 1519.487052.
+
+ic=3
+////incertitude = 3.92468 res1 = 16184.4 res2 = 16162.7 err1 = 0.109402 err2 = 5.53898////
+kID = 2, duration=1.724355s, sc = 3419.675353.
+kID = 4, duration=1.976010s, sc = 3419.675353.
+
+ic=4
+////incertitude = 3.30702 res1 = 42494 res2 = 42435.8 err1 = 2.47154 err2 = 17.4223////
+kID = 2, duration=1.714498s, sc = 7696.136341.
+kID = 4, duration=1.986156s, sc = 7696.136341.
+
+ic=5
+////incertitude = 0.755289 res1 = 101363 res2 = 101298 err1 = 61.4267 err2 = 59.8496////
+kID = 2, duration=1.709705s, sc = 17320.508076.
+kID = 4, duration=1.987070s, sc = 17320.508076.
+
+ic=6
+////incertitude = 1.08443 res1 = 226844 res2 = 227109 err1 = 99.3622 err2 = 223.594////
+kID = 2, duration=9.941635s, sc = 38980.598409.
+kID = 4, duration=1.989280s, sc = 38980.598409.
+
+ic=7
+////incertitude = 1.46595 res1 = 484654 res2 = 484230 err1 = 198.296 err2 = 210.601////
+kID = 2, duration=51.554648s, sc = 87727.625870.
+kID = 4, duration=9.983349s, sc = 87727.625870.
+
+ic=8
+////incertitude = 0.173576 res1 = 998790 res2 = 998956 err1 = 412.135 err2 = 863.099////
+kID = 2, duration=256.849330s, sc = 197435.048583.
+kID = 4, duration=9.927585s, sc = 197435.048583.
+
+ic=9
+////incertitude = 0.497842 res1 = 2.0044e+06 res2 = 2.00353e+06 err1 = 892.487 err2 = 1504.93////
+kID = 2, duration=1284.155628s, sc = 444336.638804.
+kID = 4, duration=51.818095s, sc = 444336.638804.
+
+ic=10
+////incertitude = 0.940896 res1 = 3.92625e+06 res2 = 3.92401e+06 err1 = 1864.22 err2 = 1482.27////
+kID = 2, duration=10080.063470s, sc = 1000000.000000.
+kID = 4, duration=252.878959s, sc = 1000000.000000.
+
+
+
